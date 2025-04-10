d320d3322b11065c8c167c3dc4dd7a25eccc60b4
Matyas Dostalik <dostalik303@gmail.com>
2025-04-10 12:27:21 +0200

    cteni dat funkce

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,18 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    allowed_fields = {"unordered_numbers", "ordered_numbers", "dna_sequence"}
+    if field not in allowed_fields:
+        return None
+
+    with open(file_path, mode="r") as file:
+        data = json.load(file)
+    return data[field]
+
 
 def main():
-    pass
+    numbers = read_data("sequential.json", "unordered_numbers")
+    print(numbers)
 
 
 if __name__ == '__main__':
